8d88ee6151b5404c226df65f8165e37fde4dc0d8
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2020-12-14 10:36:51 +0000

    added reading user details from db

--- src/App.js
+++ src/App.js
@@ -1,48 +1,77 @@
 import { React, Component } from 'react';
 import './App.css';
-import {auth, db, storage} from './services/firebase'
+import {db} from './services/firebase'
 import Profile from "./components/Profile"; //import user profile class
 
 
-var song = storage.ref("tracks/Downstream-ElTestigo.mp3");
+//var song = storage.ref("tracks/Downstream-ElTestigo.mp3");
 //console.log("Storageref: " + song);
 
 
 class App extends Component {
-    //set the state of the application
-    state = {
-        user: null
+    constructor(props) {
+        super(props);
+        this.state = {
+            //set the state of the application
+            //state = {
+            userId: "ljMDzpGrG3fO6T8dG7lU",
+            user: [],
+            users: []
+        };
     }
     /*wait until 'App' component mounts to the screen
       and then retrieve user's information from db*/
     componentDidMount() {
+        //const userId = "ljMDzpGrG3fO6T8dG7lU" //get the document id of logged in user
+        db.collection('users')
+          .get()
+            .then(snapshot => {
+                const users = []
+                snapshot.forEach(
+                    doc => {
+                        const data = doc.data()
+                        users.push(data)
+                    })
+              this.setState({ users: users }) }) //set App state passing the user data we just retrieved
+          .catch(error => console.log(error)) //if couldn't find user
+        //console.log(this.state.users)
+
+        //NOT WORKING
         const userId = "ljMDzpGrG3fO6T8dG7lU" //get the document id of logged in user
         db.collection('users').doc(userId)
-          .get()
-          .then(user => {
-              this.setState({ user: user.data() }) }) //set App state passing the user data we just retrieved
-          .catch(error => console.log(error)) //if couldn't find user
+            .get()
+            .then(user => {
+                //const user= null
+                console.log(user.data())
+                this.user = user.data() })
+             //set App state passing the user data we just retrieved
+            .catch(error => console.log(error)) //if couldn't find user
         console.log(this.state.user)
     }
-
-    /*getUserProfile() {
-        //console.log(this.user)
-        return new Profile({
-            avatar: this.user.avatar,
-            firstName: this.user.firstName,
-            lastName: this.user.lastName,
-            email: this.user.email,
-            age: this.user.age,
-            stylePreference: this.user.stylePreference,
-            recordings: this.user.recordings
-        })
-    }*/
 
 
     render() {
         return (
-            <div><Profile />
-                </div>
+
+            < div className="App">
+                {
+                    this.state.users &&
+                    this.state.users.map(user => {
+                        return (  
+                            //console.log(user.stylePreference),
+                            <Profile
+                                userId={this.state.userId}
+                                avatar={user.avatar}
+                                firstName={user.firstName}
+                                lastName={user.lastName}
+                                eMail={user.eMail}
+                                age={user.age}
+                                recordings={user.recordings}                                
+                                stylePreference={user.stylePreference} />
+                        )
+                    })
+                }
+            </div>
         )
     }
 }
