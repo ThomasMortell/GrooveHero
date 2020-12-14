a554fb158aa3bea5e35e3a368f6e496dac08dc1c
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2020-12-14 10:35:57 +0000

    added reading user details from db

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,26 +1,61 @@
-import avatar from '../media/avatar.png';
+//import avatar from '../media/avatar.png';
 import logo from '../media/logo.png';
 import rockcover from '../media/rock-album-cover.jpg';
 import hiphopcover from '../media/hiphop-album-cover.jpg';
 import React, { Component } from 'react';
+import { db, storage } from '../services/firebase'
 import Tabs from "./Tabs";
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
-            avatar: avatar, // this needs to later be retrieved from firebase storage based on user id
+            userId: this.props.userId,
+            avatar: this.getAvatar(this.props.userId), // this needs to later be retrieved from firebase storage based on user id
             firstName: props.firstName,
             lastName: props.lastName,
-            email: props.email,
+            eMail: props.eMail,
             age: props.age,
-            records: props.records,
-            stylePref: props.stylePref
+            recordings: props.recordings,
+            stylePreference: props.stylePreference
         };
     }
 
+    getAvatar(userId) {
+        storage.ref('user-logos')
+            .child(`${userId}.png`)
+            .getDownloadURL()
+            .then(url => {
+                this.setState({ avatar: url })
+            })
+    }
+
+    getUserDetails() {
+        console.log(this.userId)
+        db.collection('users').doc(this.userId)
+            .get()
+            .then(user => {
+                const u = user.data()
+                console.log(u)
+                this.setState({
+                    firstName: u.firstName,
+                    lastName: u.lastName,
+                    eMail: u.eMail,
+                    age: u.age,
+                    recordings: u.recordings,
+                    stylePreference: u.stylePreference
+                })
+                console.log(this.state.user)
+            })
+            .catch(error => console.log(error)) //if couldn't find user
+        console.log(this.state.user)
+    }
+
+
+
     render() {
+        console.log(this.userId)
+        this.getUserDetails(this.userId);
         return (
             <div className="user-profile-info">
                 <header className="app-header">
@@ -29,7 +64,7 @@ class Profile extends Component {
                 </header>
                 <div className="user-info">
                     <p id="my-acc-label">MY ACCOUNT</p>
-                    <img id="user-avatar" src={avatar} alt="avatar" />
+                    <img id="user-avatar" src={this.state.avatar} alt="avatar" />
                     <br></br>
                     {/*user profile information*/}
                     <label className="labels" htmlFor="fname" >First Name:</label>
@@ -37,11 +72,11 @@ class Profile extends Component {
                     <label className="labels" htmlFor="lname" >Last Name:</label>
                     <input id="lname" className="user-info-fields" type="text" value={this.props.lastName} readOnly/>
                     <label className="labels" htmlFor="email" >Email:</label>
-                    <input id="email" className="user-info-fields" type="text" value={this.props.email} readOnly/>
+                    <input id="email" className="user-info-fields" type="text" value={this.props.eMail} readOnly/>
                     <label className="labels" htmlFor="age" >Age:</label>
                     <input id="age" className="user-info-fields" type="text" value={this.props.age} readOnly/>
                     <label className="labels" htmlFor="styles-preference" >Music style preference:</label>
-                    <input id="styles-preference" className="user-info-fields" type="text" value={this.props.myStyles} readOnly/>
+                    <input id="styles-preference" className="user-info-fields" type="text" value={this.props.stylePreference} readOnly />
                 </div>
 
                 <div className="my-styles">
