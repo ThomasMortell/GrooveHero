60b4feaccb78c7ebc8251711308d803186792175
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2020-12-16 19:31:54 +0000

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var app = new App();
-app.user
+//var app = new App();
+//app.user
 ReactDOM.render(
     <React.StrictMode>
         <App />
