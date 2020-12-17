8e1dc67c22403b5f7bf44b290000b55fc84a1f2f
Mariia Skyba <mariia.skyba.2018@mumail.ie>
2020-12-17 09:00:02 +0000

    fixed user info retrieval from firebase

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,28 +1,50 @@
-//import avatar from '../media/avatar.png';
-import logo from '../media/logo.png';
+import groovehero_logo from '../media/logo.png';
 import rockcover from '../media/rock-album-cover.jpg';
 import hiphopcover from '../media/hiphop-album-cover.jpg';
 import React, { Component } from 'react';
 import { db, storage } from '../services/firebase'
 import Tabs from "./Tabs";
+import { Button, ButtonToolbar } from 'react-bootstrap';
+import UpdateProfileForm from "./UpdateProfileForm";
+
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
             userId: this.props.userId,
-            avatar: this.getAvatar(this.props.userId), // this needs to later be retrieved from firebase storage based on user id
-            firstName: props.firstName,
-            lastName: props.lastName,
-            eMail: props.eMail,
-            age: props.age,
-            recordings: props.recordings,
-            stylePreference: props.stylePreference
-        };
+            avatar: this.getAvatar(this.props.userId),
+            firstName: '',
+            lastName: '',
+            eMail: '',
+            age: '',
+            recordings: [],
+            stylePreference: [],
+            editProfileFormShow: false
+
+        };        
+    }
+
+    componentDidMount() {
+        var userRef = db.collection('users').doc(this.props.userId);
+        userRef.get()
+            .then(doc => {
+                const user = doc.data()
+
+                this.setState({
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    eMail: user.eMail,
+                    age: user.age,
+                    recordings: user.recordings,
+                    stylePreference: user.stylePreference
+                })
+            })
+            .catch(error => console.log(error))
     }
 
     getAvatar(userId) {
-        storage.ref('user-logos')
+        storage.ref('user-avatars')
             .child(`${userId}.png`)
             .getDownloadURL()
             .then(url => {
@@ -30,53 +52,49 @@ class Profile extends Component {
             })
     }
 
-    getUserDetails() {
-        console.log(this.userId)
-        db.collection('users').doc(this.userId)
-            .get()
-            .then(user => {
-                const u = user.data()
-                console.log(u)
-                this.setState({
-                    firstName: u.firstName,
-                    lastName: u.lastName,
-                    eMail: u.eMail,
-                    age: u.age,
-                    recordings: u.recordings,
-                    stylePreference: u.stylePreference
-                })
-                console.log(this.state.user)
-            })
-            .catch(error => console.log(error)) //if couldn't find user
-        console.log(this.state.user)
-    }
-
-
 
     render() {
-        console.log(this.userId)
-        this.getUserDetails(this.userId);
+
+        let editProfileFormClose = () => this.setState({ editProfileFormShow: false });
         return (
             <div className="user-profile-info">
-                <header className="app-header">
-                    <img src={logo} id="app-logo" alt="logo" />
+                <header className="groovehero-header">
+                    <img src={groovehero_logo} id="groovehero-logo" alt="groovehero" />
                     <span id="app-name">GROOVEHero</span>
                 </header>
                 <div className="user-info">
                     <p id="my-acc-label">MY ACCOUNT</p>
                     <img id="user-avatar" src={this.state.avatar} alt="avatar" />
                     <br></br>
+                    <ButtonToolbar>
+
+                        <Button
+                            id="edit-btn"
+                            onClick={()=> this.setState({ editProfileFormShow: true })}>
+                        </Button>
+                        
+                        <UpdateProfileForm
+                            userId={this.state.userId}
+                            firstName={this.state.firstName}
+                            lastName={this.state.lastName}
+                            age={this.state.age}
+                            stylePreference={this.state.stylePreference}
+                            show={this.state.editProfileFormShow}
+                            onHide={editProfileFormClose} /> 
+                        
+                    </ButtonToolbar>
+
                     {/*user profile information*/}
                     <label className="labels" htmlFor="fname" >First Name:</label>
-                    <input id="fname" className="user-info-fields" type="text" value={this.props.firstName} readOnly/>
+                    <input id="fname" className="user-info-fields" type="text" value={this.state.firstName} readOnly/>
                     <label className="labels" htmlFor="lname" >Last Name:</label>
-                    <input id="lname" className="user-info-fields" type="text" value={this.props.lastName} readOnly/>
+                    <input id="lname" className="user-info-fields" type="text" value={this.state.lastName} readOnly/>
                     <label className="labels" htmlFor="email" >Email:</label>
-                    <input id="email" className="user-info-fields" type="text" value={this.props.eMail} readOnly/>
+                    <input id="email" className="user-info-fields" type="text" value={this.state.eMail} readOnly/>
                     <label className="labels" htmlFor="age" >Age:</label>
-                    <input id="age" className="user-info-fields" type="text" value={this.props.age} readOnly/>
+                    <input id="age" className="user-info-fields" type="text" value={this.state.age} readOnly/>
                     <label className="labels" htmlFor="styles-preference" >Music style preference:</label>
-                    <input id="styles-preference" className="user-info-fields" type="text" value={this.props.stylePreference} readOnly />
+                    <input id="styles-preference" className="user-info-fields" type="text" value={this.state.stylePreference} readOnly />
                 </div>
 
                 <div className="my-styles">
@@ -102,6 +120,7 @@ class Profile extends Component {
                         </div>
                     </Tabs>
                 </div>
+
             </div>);
     }
 }
